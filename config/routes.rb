3cfdefb054a8c90ce7f3914199ff45cba8d504d5
satub <satu.barnhill@gmail.com>
2016-09-11 14:04:21 -0400

    Done

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,11 @@ Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
   root 'posts#index'
+
+  # get '/admin/stats', to: 'stats#index'
+
+  namespace :admin do
+    resources :stats, only: [:index]
+  end
+
 end
