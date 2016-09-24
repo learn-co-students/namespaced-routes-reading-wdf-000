ed07b6ce9aaffae233e837a31f775894edb28992
fidelscodes <fidelscodes@gmail.com>
2016-09-24 16:10:29 -0400

    update git tracking

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,15 @@ Rails.application.routes.draw do
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
+  # get '/admin/stats', to: 'stats#index'
+  # get '/admin/authors/new', to: 'authors#new'
+  # get '/admin/authors/delete', to: 'authors#delete'
+  # get '/admin/authors/create', to: 'authors#create'
+  # get '/admin/comments/moderate', to: 'comments#moderate'
+
+  # scope allows us to group all the routes above under /admin
+  scope '/admin' do
+    resources :stats, only: [:index]
+  end
   root 'posts#index'
 end
