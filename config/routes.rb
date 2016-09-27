aaffce442aabe15bda9c597a53a0f62f65c166cb
Flatiron School <github email address>
2016-09-27 16:59:48 -0400

    done

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,10 @@ Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
   root 'posts#index'
+
+  # get 'admin/stats', to: 'stats#index' shorter vvv
+  # scope '/admin', module: 'admin' do
+  namespace :admin do 
+    resources :stats, only: [:index]
+  end
 end
